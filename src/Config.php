744e78468ded65889c86a2cfd015b1ae3a3cd5b1
Thomas Müller <thomas.mueller@tmit.eu>
2018-05-15 17:08:09 +0200

    setIntent to tabs and extend rules to support PSR2

--- src/Config.php
+++ src/Config.php
@@ -29,18 +29,48 @@ class Config extends BaseConfig {
 	public function __construct($name = 'default')
 	{
 		parent::__construct('ownCloud coding standard');
+		$this->setIndent("\t");
 	}
 
 	public function getRules()
 	{
 		$rules = [
-			'native_function_invocation' => true,
-			'@PSR1' => true,
-			'array_syntax' => ['syntax' => 'short'],
-			'combine_consecutive_issets' => true,
-			'combine_consecutive_unsets' => true,
-			'no_spaces_inside_parenthesis' => true,
-			'no_extra_consecutive_blank_lines' => true,
+            '@PSR1' => true,
+            // PRS-2
+            'braces' => [
+                'position_after_functions_and_oop_constructs' => 'same'
+            ],
+            'class_definition' => true,
+            'elseif' => true,
+            'encoding' => true,
+            'full_opening_tag' => true,
+            'function_declaration' => true,
+            'indentation_type' => true,
+            'line_ending' => true,
+            'lowercase_constants' => true,
+            'lowercase_keywords' => true,
+            'method_argument_space' => true,
+            'no_break_comment' => true,
+            'no_closing_tag' => true,
+            'no_spaces_after_function_name' => true,
+            'no_spaces_inside_parenthesis' => true,
+            'no_trailing_whitespace' => true,
+            'no_trailing_whitespace_in_comment' => true,
+            'single_blank_line_at_eof' => true,
+            'single_class_element_per_statement' => true,
+            'single_import_per_statement' => true,
+            'single_line_after_imports' => true,
+            'switch_case_semicolon_to_colon' => true,
+            'switch_case_space' => true,
+            'visibility_required' => true,
+            // ownCloud specific
+            'native_function_invocation' => true,
+            'array_syntax' => ['syntax' => 'short'],
+            'combine_consecutive_issets' => true,
+            'combine_consecutive_unsets' => true,
+            'no_extra_consecutive_blank_lines' => true,
+            'is_null' => true,
+            'yoda_style' => false
 		];
 		return $rules;
 	}
